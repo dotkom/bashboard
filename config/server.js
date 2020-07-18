0836db20c3ab1b26c8274cb83385b04c076c0ab5
kharann <anhkhav@gmail.com>
2020-07-19 01:32:40 +0200

    fix cms config

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  cron: { enabled: false },
+  admin: { autoOpen: false },
+  url: "http://localhost:1337",
 });
