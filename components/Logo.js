ffa00c18b42851e7799e8924c0a5268aee393095
kharann <anhkhav@gmail.com>
2020-07-19 17:15:33 +0200

    Add Online Logo

---
+++ components/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./logo.css";
+
+const Logo = () => (
+  <img src="../static/online.png" alt="my logo" className={styles.logo} />
+);
+
+export default Logo;
