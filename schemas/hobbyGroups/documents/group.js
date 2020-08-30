597c3364287d740b1982d65ffce548565da3f63c
kharann <anhkhav@gmail.com>
2020-08-30 14:50:36 +0200

    Add mor efields to interest group

--- schemas/hobbyGroups/documents/group.js
+++ schemas/hobbyGroups/documents/group.js
@@ -1,27 +1,35 @@
 export default {
-    name: 'hobbyGroup',
-    type: 'document',
-    title: 'Interessegruppe',
-    fields: [
-        {
-            name: 'name',
-            type: 'string',
-            title: 'Navn',
-        },
-        {
-            name: 'description',
-            type: 'text',
-            title: 'Beskrivelse',
-        },
-        {
-            name: 'image',
-            type: 'image',
-            title: 'Bilde',
-        },
-        {
-            name: "active",
-            type: "boolean",
-            title: "Aktiv",
-        }
-    ]
-}
+  name: "hobbyGroup",
+  type: "document",
+  title: "Interessegruppe",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Navn",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Beskrivelse",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Bilde",
+    },
+    {
+      name: "read_more_link",
+      type: "link",
+      title: "Les mer lenke",
+    },
+    {
+      name: "active",
+      type: "boolean",
+      title: "Aktiv",
+    },
+  ],
+  default: () => ({
+    active: false,
+  }),
+};
