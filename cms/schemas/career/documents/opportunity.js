1483648c262bd510feb5e3b2e897f10597753f1b
kharann <anhkhav@gmail.com>
2021-05-02 14:49:34 +0200

    prettify

--- cms/schemas/career/documents/opportunity.js
+++ cms/schemas/career/documents/opportunity.js
@@ -1,82 +1,91 @@
 export default {
-    name: 'careerOpportunity',
-    type: 'document',
-    title: 'Karrieremulighet',
-    fields: [
-        {
-            name: 'title',
-            type: 'string',
-            title: 'Tittel',
-            description: 'Tittel på arbeidsmuligheten'
-        },
-        {
-            name: 'ingress',
-            type: 'string',
-            title: 'Ingrees',
-            description: 'En kort ingress'
-        },
-        {
-            name: 'description',
-            type: 'text',
-            title: "Beskrivelse",
-            description: 'Beskrivelse av arbeidsmuligheten'
-        },
-        {
-            title: 'Startdato',
-            name: 'startDate',
-            type: 'date',
-            description: "Arbeidsmulighetens startdato"
-        },
-        {
-            title: 'Sluttdato',
-            name: 'endDate',
-            type: 'date',
-            description: "Arbeidsmulighetens sluttdato"
-        },
-        {
-            title: 'Søknadsfrist',
-            name: 'deadline',
-            type: 'date',
-            description: "Frist for å søke"
-        },
-        {
-            title: 'Søknadslenke',
-            name: 'applicationUrl',
-            type: 'url',
-            description: "Link til søknadsside"
+  name: "careerOpportunity",
+  type: "document",
+  title: "Karrieremulighet",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Tittel",
+      description: "Tittel på arbeidsmuligheten",
+    },
+    {
+      name: "ingress",
+      type: "string",
+      title: "Ingrees",
+      description: "En kort ingress",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Beskrivelse",
+      description: "Beskrivelse av arbeidsmuligheten",
+    },
+    {
+      title: "Startdato",
+      name: "startDate",
+      type: "date",
+      description: "Arbeidsmulighetens startdato",
+    },
+    {
+      title: "Sluttdato",
+      name: "endDate",
+      type: "date",
+      description: "Arbeidsmulighetens sluttdato",
+    },
+    {
+      title: "Søknadsfrist",
+      name: "deadline",
+      type: "date",
+      description: "Frist for å søke",
+    },
+    {
+      title: "Søknadslenke",
+      name: "applicationUrl",
+      type: "url",
+      description: "Link til søknadsside",
+    },
+    {
+      title: "Sted",
+      name: "location",
+      type: "geopoint",
+      description: "Arebeidsmulighetens plassering",
+    },
+    {
+      title: "Type",
+      name: "type",
+      type: "string",
+      options: {
+        list: [
+          {
+            title: "Heltid",
+            value: "fullTime",
           },
           {
-            title: 'Sted',
-            name: 'location',
-            type: 'geopoint',
-            description: "Arebeidsmulighetens plassering"
+            title: "Deltid",
+            value: "partTime",
           },
           {
-            title: 'Type',
-            name: 'type',
-            type: 'string',
-            options: {
-                list: [
-                  {
-                    title: 'Heltid',
-                    value: 'fullTime'
-                  },
-                  {
-                    title: 'Deltid',
-                    value: 'partTime'
-                  },
-                  {
-                    title: 'Sommerjobb',
-                    value: 'summerJob'
-                  }
-                ]
-              }
+            title: "Sommerjobb",
+            value: "summerJob",
           },
-          {
-            title: 'Bedrift',
-            name: 'company',
-            type: 'reference',
-            to: [{type: 'company'}]
-          }
-    ]
-}
+        ],
+      },
+    },
+    {
+      title: "Bedrift",
+      name: "company",
+      type: "reference",
+      to: [{ type: "company" }],
+    },
+    {
+      title: "Fremhevet",
+      name: "featured",
+      type: "boolean",
+      description: "Er muligheten fremhevet?",
+    },
+  ],
+  initialValue: {
+    featured: false,
+  },
+};
